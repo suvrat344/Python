fedae68a96df38d2eb8de0d55a7ff1d96557782c
Suvrat344 <chauhansuvrat344@gmail.com>
2024-06-21 23:57:12 +0530

    Looping Question

---
+++ L13Looping/p14CheckPrime.py
@@ -0,0 +1,23 @@
+# Accept a positive integer n as input, where n>1. Print PRIME if n is a prime number and NOTPRIME otherwise.
+# Input                               Expected Output
+# Test Case1
+# 11                                     PRIME
+# Test Case2
+# 17                                     PRIME
+
+n = int(input("Enter a positive integer : "))
+isprime = 0
+if(n > 1):
+    for i in range(2,n//2+1):
+        if(n%i==0):
+            isprime = 0
+            break
+        else:
+            isprime = 1
+else:
+    print("NOTPRIME")
+    
+if(isprime or n==2):
+    print("PRIME")
+else:
+    print("NOTPRIME")
---
+++ L13Looping/p19PrintPattern.py
@@ -0,0 +1,18 @@
+# Accept a positive integer n as input and print a triangle of zeros for n lines. The ith line should have i zeros. There shouldn't be any spaces between consecutive zeros. Do not print a space at the end of a line.
+# Input                   Expected Output
+# Test Case1
+# 4                           0
+#                             00
+#                             000
+#                             0000
+# Test Case2
+# 6                           0
+#                             00
+#                             000
+#                             0000
+#                             00000
+#                             000000
+
+n = int(input("Enter a positive integer : "))
+for i in range(1,n + 1):
+    print("0"*i)
